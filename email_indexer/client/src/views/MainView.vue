54d795e5c2f11596745087619f19fabfd65c005d
Juan Felipe Jaramillo <juanfelipejaramillolosada@gmail.com>
2024-03-19 16:35:18 -0500

    front

---
+++ email_indexer/client/src/views/MainView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <NavbarDark />
+  </div>
+</template>
+
+<script>
+import NavbarDark from "../components/NavbarDark.vue";
+
+export default {
+  components: {
+    NavbarDark
+  },
+  data() {
+    return {};
+  }
+};
+</script>
